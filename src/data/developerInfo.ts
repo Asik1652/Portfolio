7d077fa8550a534d27af764e63c7fed737d46999
Asik Umar <asikumar.tk@techgenzi.com>
2026-02-26 19:11:41 +0530

    potfolio update

--- src/data/developerInfo.ts
+++ src/data/developerInfo.ts
@@ -6,7 +6,7 @@ export const developerInfo = {
   phone: "+91 63800-54818",
   linkedIn: "https://www.linkedin.com/in/asik-umar-9b5269225",
   github: "https://github.com/Asik1652",
-  resume: "https://drive.google.com/file/d/1knWpzs7Te6fNjNCVuXwnJ7RguxsN_Tpl/view?usp=sharing", // Replace with actual file or Google Drive URL
+  resume: "https://rxresu.me/asikumar327/my-resume", // Replace with actual file or Google Drive URL
   about: `I'm a React Native Developer with hands-on experience building full-scale B2B mobile applications like CRM, CMS, and FEMS in a fast-paced startup. I take full ownership of my work — from UI development to API integration — and specialize in crafting scalable, user-friendly mobile experiences. Currently expanding into full-stack web with MERN.`,
   lookingFor: `Remote opportunities (preferably from Bangalore-based companies) where I can contribute to product development, learn full-stack web, and grow with high-performance teams.`
 }; 
